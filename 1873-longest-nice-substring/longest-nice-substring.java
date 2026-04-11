91260c4e6f951254bcfd8ebdcbdc6d65fc9f6277
Rajeswaran S <rajeswaranseenipandian2005@gmail.com>
2026-04-11 21:55:05 +0530

    Time: 27 ms (21.20%) | Memory: 46.2 MB (25.60%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        int stringLength = s.length();
+        int startIndex = -1; 
+        int maxLength = 0;    
+        for (int i = 0; i < stringLength; ++i) {
+            Set<Character> charSet = new HashSet<>();
+            for (int j = i; j < stringLength; ++j) {
+                charSet.add(s.charAt(j));
+                boolean isNice = true;
+                for (char ch : charSet) {
+                   char counterpart = (char) (ch ^ 32);
+                    if (!charSet.contains(ch) || !charSet.contains(counterpart)) {
+                        isNice = false;
+                        break;
+                    }
+                }
+                if (isNice && maxLength < j - i + 1) {
+                    maxLength = j - i + 1;
+                    startIndex = i;
+                }
+            }
+        }
+        return startIndex == -1 ? "" : s.substring(startIndex, startIndex + maxLength);
+    }
+}
