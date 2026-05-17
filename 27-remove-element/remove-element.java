6d884d798cb09d753a4c0bde5e95967945dae0d7
Rajeswaran S <rajeswaranseenipandian2005@gmail.com>
2026-05-17 20:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.49%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+  
+    public int removeElement(int[] nums, int val) {
+        int validElementIndex = 0;
+        for (int currentElement : nums) {
+            if (currentElement != val) {
+                nums[validElementIndex] = currentElement;
+                validElementIndex++;
+            }
+        }
+      
+        return validElementIndex;
+    }
+}
